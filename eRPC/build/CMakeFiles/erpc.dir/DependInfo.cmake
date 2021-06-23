1a3e6a58d29a591bd412144ee29e96e7340e15fb
dgiantsidi <dimitra.giantsidi@gmail.com>
2021-06-23 10:45:21 +0000

    eRPC & dpdk-scone

---
+++ eRPC/build/CMakeFiles/erpc.dir/DependInfo.cmake
@@ -0,0 +1,59 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/src/nexus_impl/nexus.cc" "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/build/CMakeFiles/erpc.dir/src/nexus_impl/nexus.cc.o"
+  "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/src/nexus_impl/nexus_bg_thread.cc" "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/build/CMakeFiles/erpc.dir/src/nexus_impl/nexus_bg_thread.cc.o"
+  "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/src/nexus_impl/nexus_sm_thread.cc" "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/build/CMakeFiles/erpc.dir/src/nexus_impl/nexus_sm_thread.cc.o"
+  "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/src/rpc_impl/rpc.cc" "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/build/CMakeFiles/erpc.dir/src/rpc_impl/rpc.cc.o"
+  "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/src/rpc_impl/rpc_connect_handlers.cc" "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/build/CMakeFiles/erpc.dir/src/rpc_impl/rpc_connect_handlers.cc.o"
+  "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/src/rpc_impl/rpc_cr.cc" "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/build/CMakeFiles/erpc.dir/src/rpc_impl/rpc_cr.cc.o"
+  "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/src/rpc_impl/rpc_disconnect_handlers.cc" "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/build/CMakeFiles/erpc.dir/src/rpc_impl/rpc_disconnect_handlers.cc.o"
+  "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/src/rpc_impl/rpc_ev_loop.cc" "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/build/CMakeFiles/erpc.dir/src/rpc_impl/rpc_ev_loop.cc.o"
+  "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/src/rpc_impl/rpc_fault_inject.cc" "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/build/CMakeFiles/erpc.dir/src/rpc_impl/rpc_fault_inject.cc.o"
+  "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/src/rpc_impl/rpc_kick.cc" "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/build/CMakeFiles/erpc.dir/src/rpc_impl/rpc_kick.cc.o"
+  "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/src/rpc_impl/rpc_pkt_loss.cc" "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/build/CMakeFiles/erpc.dir/src/rpc_impl/rpc_pkt_loss.cc.o"
+  "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/src/rpc_impl/rpc_queues.cc" "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/build/CMakeFiles/erpc.dir/src/rpc_impl/rpc_queues.cc.o"
+  "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/src/rpc_impl/rpc_req.cc" "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/build/CMakeFiles/erpc.dir/src/rpc_impl/rpc_req.cc.o"
+  "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/src/rpc_impl/rpc_reset_handlers.cc" "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/build/CMakeFiles/erpc.dir/src/rpc_impl/rpc_reset_handlers.cc.o"
+  "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/src/rpc_impl/rpc_resp.cc" "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/build/CMakeFiles/erpc.dir/src/rpc_impl/rpc_resp.cc.o"
+  "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/src/rpc_impl/rpc_rfr.cc" "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/build/CMakeFiles/erpc.dir/src/rpc_impl/rpc_rfr.cc.o"
+  "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/src/rpc_impl/rpc_rx.cc" "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/build/CMakeFiles/erpc.dir/src/rpc_impl/rpc_rx.cc.o"
+  "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/src/rpc_impl/rpc_sm_api.cc" "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/build/CMakeFiles/erpc.dir/src/rpc_impl/rpc_sm_api.cc.o"
+  "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/src/rpc_impl/rpc_sm_helpers.cc" "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/build/CMakeFiles/erpc.dir/src/rpc_impl/rpc_sm_helpers.cc.o"
+  "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/src/transport_impl/dpdk/dpdk_transport.cc" "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/build/CMakeFiles/erpc.dir/src/transport_impl/dpdk/dpdk_transport.cc.o"
+  "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/src/transport_impl/dpdk/dpdk_transport_datapath.cc" "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/build/CMakeFiles/erpc.dir/src/transport_impl/dpdk/dpdk_transport_datapath.cc.o"
+  "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/src/transport_impl/infiniband/ib_transport.cc" "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/build/CMakeFiles/erpc.dir/src/transport_impl/infiniband/ib_transport.cc.o"
+  "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/src/transport_impl/infiniband/ib_transport_datapath.cc" "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/build/CMakeFiles/erpc.dir/src/transport_impl/infiniband/ib_transport_datapath.cc.o"
+  "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/src/transport_impl/raw/raw_transport.cc" "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/build/CMakeFiles/erpc.dir/src/transport_impl/raw/raw_transport.cc.o"
+  "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/src/transport_impl/raw/raw_transport_datapath.cc" "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/build/CMakeFiles/erpc.dir/src/transport_impl/raw/raw_transport_datapath.cc.o"
+  "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/src/transport_impl/transport.cc" "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/build/CMakeFiles/erpc.dir/src/transport_impl/transport.cc.o"
+  "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/src/util/externs.cc" "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/build/CMakeFiles/erpc.dir/src/util/externs.cc.o"
+  "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/src/util/huge_alloc.cc" "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/build/CMakeFiles/erpc.dir/src/util/huge_alloc.cc.o"
+  "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/src/util/tls_registry.cc" "/home/dimitra/workspace/anchor/network-stack/client_server_twosided/eRPC/build/CMakeFiles/erpc.dir/src/util/tls_registry.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ERPC_DPDK=true"
+  "ERPC_LOG_LEVEL=2"
+  "ERPC_TESTING=false"
+  "NDEBUG"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "../third_party"
+  "../dpdk_scone/usr/include/dpdk"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
